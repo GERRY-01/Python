5d492f0af81d9475119a714ba1bb21c1d31f697e
Gerry-01 <odhiambogerry010@gmail.com>
2025-03-10 11:43:59 +0300

    isprime

---
+++ isprime.py
@@ -0,0 +1,12 @@
+num = int(input("Enter any number: "))
+
+if num < 2 :
+    print("Not a prime number")
+
+else :
+    for i in range(2,num) :
+        if num % i == 0 :
+            print("Not a prime number")
+            break
+    else :
+        print(" A prime number")
